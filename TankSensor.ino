1cd0ed3e6e59b98f33ccf1dfdb9d893dca1ce9c4
John Hughes <jfh@cs.brown.edu>
2024-11-16 10:18:17 -0500

    Check functionality for all possible modes (Wifi connects or not, power-mode startup or no). Get rid of OTA (except through "setup" menu perhaps).

--- TankSensor.ino
+++ TankSensor.ino
@@ -1,3 +1,27 @@
+/*
+Code cleanup, testing, and documentation
+   - .Define “power mode”
+   - .“edit” and buttons and “setup” available in power mode
+     xMake OTA active when touch-sensor is touched
+
+Testing
+.Start up in non-power mode; do buttons function? No, they don't, so OK. 
+Startup in non-power mode bad wifi name; 
+  .does portal appear when I go to esp-...? (Yes, but you have to go to the ESP Wifi network first)
+  Does portal open and work?  yes
+Startup in power mode and bad wifi name
+  .does portal appear when I go to esp-...? (Yes, but you have to go to the ESP Wifi network first)
+  .Does portal open and work? 
+Startup with good wifi, no power mode
+  Is portal and edit hidden? yes, at least on second restart
+  Is DNS name working? Yes
+  Is the sensor value display correct? Yes
+
+Startup with good wifi, touch-sensor on: 
+   am I in power mode? yes
+   Does DNS name work? yes
+
+*/
 #pragma once
 
 #ifndef TANK_SENSOR_ino
@@ -10,40 +34,11 @@
 #include "OTA.h"
 #include "Persistence.h"
 #include "TouchAndSense.h"
+#include "Tank.h"
 #include "Averaging.h"
 #include <esp_task_wdt.h>
 
-
-/* General notes 
-This project will use a 4-20mA sensor to measure tank-fullness. There are several goals:
-* Enter "empty" and "full" readings via the buttons on the EPS32 for initial setup
-* In response to a touchpad touch, turn on the display and show a "level" gauge to give a visual sense of fullness
-* Have that display turn off 19 seconds after the touuch is released
-* Have the device connect to a local network with a default web page that shows how full the tank is
-*
-* ESP pinouts: https://sites.google.com/site/jmaathuis/arduino/lilygo-ttgo-t-display-esp32#h.p_uUP08r1njLqE
-*
-* The ESP-32 has two A AsyncFsWebServer units; the second one is used by the Wifi subsysten
-* (see https://docs.espressif.com/projects/esp-idf/en/release-v4.4/esp32/api-reference/peripherals/adc.html)
-* so we have to use the first (ADC1), which supports pins 32-39; we'll use pin 36 ("SVP" or "SENSOR_VP", where SVP and SVN are 
-* names for a sensor measurement component that's now deprecated. Pin 36 is input only, with no pull-up or pull-down. 
-*
-* For touch-sensitive stuff,  we'll use pin 32, "TOUCH9" (which may be swapped with GPIO 33, depending on updates, so maybe we need to refer to 
-* GPIO32 as T8 rather than T9)
-*
-* Temporarily, we'll use "touchPinWakeup" (pin 33) to signal that we should awake the display for 5 seconds, say. 
-*
-* For the low- and high-level setting feature, we'll use the left and right buttons, GPIO0 and GPIO35. 
-*
-* There's an LED temporarily attached to pin 2 to give feedback during development
-*
-* To avoid flicker, we'll use a sprite (see https://www.youtube.com/watch?v=sRGXQg7028A)
-* 
-* Also: OTA programming: https://lastminuteengineers.com/esp32-ota-updates-arduino-ide/
-*/
-
 // Wifi network to try initially
-
 const char* defaultWifiSSID = "DCFBoat2";
 const char* defaultWifiPassword = "Prudence";
 
@@ -52,11 +47,13 @@ const int serialSpeed = 115200;
 bool awake = false; // true only when display should be on
 int lastTouchMillis = -1; // the "millis" value when the touch-sensor was last touched
 const int interval = 10000; // 10 seconds of awakeness after most recent touch
+const int OTATimeout = 30000; // 30 seconds of opportunity to do over-the-air programming in power mode. 
+static bool powerMode = false; // allow the editor and setup dialog to be accessed
 
-static bool show_editor = false;
 String getDefaultWifiSSID(){
   return defaultWifiSSID;
 }
+
 String getDefaultWifiPassword(){
   return defaultWifiPassword;
 }
@@ -83,45 +80,42 @@ void setup() {
 //  displayMessage("Averaging", 1);
   delay(1000);
   if (getTouchState()){
-    show_editor = true;
-    Serial.println("Editor and Setup enabled!");  
- //   displayMessage("Editor on!", 1);
+    powerMode = true;
+    displayMessage("Power-mode on!", 1);
   }
-  webInit(show_editor);
+  webInit(powerMode);
   Serial.println("Web init OK");
 //  displayMessage("WebServer", 1);
-  OTAInit();
-  Serial.println("OTA init OK");
+/*  if (powerMode) {
+    OTAInit();
+    Serial.println("OTA init OK");
 //  displayMessage("OTA", 1);
+  }
+  */
   displayText("PW: " + String(getPassword()));
   delay(4000);
   displayActivate(false);
 }
 
 void loop() {
-  static int new_reading;
-
-  button_loop();  // check whether the on-board buttons have been pressed
-  if (getTouchState()){
+  if (powerMode) button_loop();  // check whether the on-board buttons have been pressed
+  
+/*  if (powerMode && (millis() < OTATimeout)) {
     ArduinoOTA.handle();
   }
-  int s = getSenseValue();
-  new_reading = updateAverage(s+ random(-5, 6)); //FIX THIS TO REMOVE RANDOMNESS! 
-  //displayShowLevelBar(new_reading, getLowerLimit(), getUpperLimit(), getCriticalValue());
-  displayShowLevelGauge(new_reading, getLowerLimit(), getUpperLimit(), getCriticalValue(), getCriticalDirection());
+*/
+  displayShowLevelGauge(getTankLevelPercent(), getCriticalLevelPercent(), getCriticalDirection());
 
   if (getTouchState()) { // if the touch-pad is being touched
     awake = true; 
     lastTouchMillis = millis(); 
     displayActivate(true);
-   // esp_task_wdt_reset();
   }
   else if (awake) {
     if (millis() - lastTouchMillis > interval) {
       awake = false; 
       lastTouchMillis = -1;
       displayActivate(false);
-    //  esp_task_wdt_reset();
     }
   } 
   
